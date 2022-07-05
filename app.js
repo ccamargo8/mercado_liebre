5b1ddea04b720b34b9b2695460646625676edf6b
ccamargo8 <68198163+ccamargo8@users.noreply.github.com>
2022-07-04 19:07:38 -0500

    Se agrega variable de entorno para Heroku en app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3030, () => {
+app.listen(process.env.PORT || 3030, () => {
   console.log('Listening on port 3030');
 });
 
